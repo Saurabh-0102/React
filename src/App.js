d89875832b81ad42ec1d984f2d5bf80d60f84ae8
saurabh-0102 <jharia.saurabh75@gmail.com>
2022-01-03 23:47:25 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 
-export default function app(){
 
-    return (
+import New from "./Components/New";
+
+export default function app() {
+  return (
+    <div>
+      <div>
         <h1>Hello world</h1>
-    )
-};
+        <New></New>
+      </div>
+      <div>
+        <Para></Para>
+      </div>
+    </div>
+  );
+}
+
+
+export function Para(){
+    return(
+        <div className="col">
+        <p>Hello every one</p>
+    </div>
+    )     
+}
 
---
+++ src/Components/New.js
@@ -0,0 +1,8 @@
+export default function New() {
+  return (
+    <div>
+      <h3>This is new tag</h3>
+      <h3>Which is user defined</h3>
+    </div>
+  );
+}

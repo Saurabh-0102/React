d19a6f463d363d21d6f0cf3e9698ad22bb9374e4
saurabh-0102 <jharia.saurabh75@gmail.com>
2022-01-04 13:34:55 +0530

    Increment operation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function App() {
   return (
     <>
-      <Products title="Rose" description="Wedding flower" Likes="5000" price = "13rs" />
-      <Products title="Lotus" description="Function flower" Likes="6000" price = "14rs" />
-      <Products title="Lily" description="Festival flower" Likes="7000" price = "15rs" />
+      <MyComponent />
     </>
   );
 }
 
-function MyCompenent() {
+function MyComponent() {
+  let [counter, setcounter] = useState(0);
+
+  const increment = () => {
+      counter = counter + 1;
+
+      setcounter(counter);
+  };
+
   return (
     <div>
-      <h1>Working with input elements</h1>
-      <input className="form-control" type="text" placeholder="Tweet here..." />
-      <input type="button" value="Tweet" />
+      <h4>Hello</h4>
+      <input type="text" value="Type here" />
+      <input type="button" value="Chick Me" onClick={increment} />
+      <div>
+          Like {counter}
+      </div>
     </div>
   );
-}
-
-// function Products() {
-//   return (
-//     <div>
-//       <h4> Flower details</h4>
-//       {/* <p>Title : {this.props.title}</p> */}
-//     </div>
-//   );
-// }
-
-class Products extends React.Component {
-  render() {
-    return (
-      <div>
-        <h3>Flower details</h3>
-        <p> Title : {this.props.title} </p>
-        <p> Description : {this.props.description} </p>
-        <p> Likes : {this.props.title} </p>
-        <p> Likes : {this.props.price} </p>
-      </div>
-    );
-  }
 }

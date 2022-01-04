cbccd1d7b70c77890669384a937c7b7fcec34249
saurabh-0102 <jharia.saurabh75@gmail.com>
2022-01-05 00:14:06 +0530

    Ajex call with react

--- src/App.js
+++ src/App.js
@@ -9,88 +9,25 @@ export default function App() {
 }
 
 function MyComponent() {
-  const [validate,setvalidate] = useState(false);
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
-  const [number, setnumber] = useState("");
+  
+  const [ajexList,setAjexList] = useState([]);
 
-  const [userlist, setuser] = useState([]);
-
-
-
-  const proemail = (e) => {
-    setemail(e.target.value);
+  const ajexCall = async () => {
+     const url = "https://jsonplaceholder.typicode.com/posts";
+     const response = await fetch(url);
+     const res = await response.json();
+     setAjexList(res);
   };
 
-  const propass = (e) => {
-    setpassword(e.target.value);
-  };
-
-  const pronum = (e) => {
-    setnumber(e.target.value);
-  };
-
-  const register = () => {
-    
-  if(email == ""|| password == "" || number == "" ){
-    setvalidate(true);
-    return;
-  }
-
-    const user = {
-      email: email,
-      password: password,
-      number: number,
-    };
-    const newuser = [user, ...userlist];
-    setuser(newuser);
-    setemail("");
-    setpassword("");
-    setnumber("");
-
-    setvalidate(false);
-  };
-
-  return (
-    <div>
+    return (
       <div>
-        <input
-          type="text"
-          placeholder="Enter Email"
-          value={email}
-          onChange={proemail}
-          className={ email == "" && validate  ? "border border-danger" : ""}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Password"
-          value={password}
-          onChange={propass}
-          className={ email == "" && validate  ? "border border-danger" : ""}
+        <h3>Hello react and ajex</h3>
+        <input type="button" value="Get data" onClick={ajexCall} />
 
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Enter Mobile Number"
-          value={number}
-          onChange={pronum}
-          className={ email == "" && validate  ? "border border-danger" : ""}
-
-        />
-      </div>
-      <div>
-        <input type="button" value="Submit" onClick={register} />
-      </div>
-
-      {userlist.map((item) => (
-        <div>
-          {item.email}, {item.password}, {item.number}{" "}
-        </div>
+      {ajexList.map((item) => (
+         <div> {item.title} </div> 
+         
       ))}
-    </div>
-  );
+      </div>
+    )
 }

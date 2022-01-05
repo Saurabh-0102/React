276d3e3e5b3286842f6be40cda1c279a57c2fff9
saurabh-0102 <jharia.saurabh75@gmail.com>
2022-01-05 20:50:45 +0530

    route

--- src/App.js
+++ src/App.js
@@ -3,31 +3,8 @@ import { useState } from "react";
 export default function App() {
   return (
     <>
-      <MyComponent />
+      
     </>
   );
 }
 
-function MyComponent() {
-  
-  const [ajexList,setAjexList] = useState([]);
-
-  const ajexCall = async () => {
-     const url = "https://jsonplaceholder.typicode.com/posts";
-     const response = await fetch(url);
-     const res = await response.json();
-     setAjexList(res);
-  };
-
-    return (
-      <div>
-        <h3>Hello react and ajex</h3>
-        <input type="button" value="Get data" onClick={ajexCall} />
-
-      {ajexList.map((item) => (
-         <div> {item.title} </div> 
-         
-      ))}
-      </div>
-    )
-}
